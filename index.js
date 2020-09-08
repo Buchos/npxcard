ee5e272bf8c278ef017ee6cc025cc488ebadc7f2
Buchos <pluyart@gmail.com>
2020-09-08 10:11:20 +0200

    creation

---
+++ index.js
@@ -0,0 +1,60 @@
+
+#!/usr/bin / env node
+/* buche/npxcard
+ *
+ * /index.js - Main Entry point
+ *
+ * coded by buche!
+ * refactored at 27/08/2019
+ */
+
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+// Text + chalk definitions
+const data = {
+    name: chalk.white("             Arthur PLUYMERS"),
+    handle: chalk.white("buche"),
+    work: chalk.white("Code Guardian at BeCode.org"),
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("buche_be"),
+    npm: chalk.gray("https://npmjs.com/") + chalk.red("~buche"),
+    github: chalk.gray("https://github.com/") + chalk.green("buchos"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("arthur.pluymers"),
+    web: chalk.cyan("https://buche.me"),
+    npx: chalk.red("npx") + " " + chalk.white("buche"),
+    labelWork: chalk.white.bold("       Work:"),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelNpm: chalk.white.bold("        npm:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:"),
+};
+
+const NEWLINE = "\n";
+const EMPTYLINE = "";
+
+console.log(
+    chalk.green(
+        boxen(
+            [
+                `${data.name} / ${data.handle}`,
+                EMPTYLINE,
+                `${data.labelWork} ${data.work}`,
+                EMPTYLINE,
+                `${data.labelTwitter} ${data.twitter}`,
+                `${data.labelNpm} ${data.npm}`,
+                `${data.labelGitHub} ${data.github}`,
+                `${data.labelLinkedIn} ${data.linkedin}`,
+                `${data.labelWeb} ${data.web}`,
+                EMPTYLINE,
+                `${data.labelCard} ${data.npx}`,
+            ].join(NEWLINE),
+            {
+                padding: 1,
+                margin: 1,
+                borderStyle: "round",
+            },
+        ),
+    ),
+);
